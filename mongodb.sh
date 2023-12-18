4475cf7d9789d089afa9a2b6365b412412eba67d
Brahmamdevops <brahmamm32@gmail.com>
2023-12-18 14:30:33 +0530

    mongodb

---
+++ mongodb.sh
@@ -0,0 +1,52 @@
+#1/bin/bash
+
+ID=(id -u)
+
+echo " $0 is started"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo : -e " $R  $2 error $N"
+    else
+        echo : " $G  $2 success $N"
+    fi
+}
+
+
+if [ $ID -ne 0 ]
+then
+    echo : "please install with root user"
+    exit 1
+else
+    echo : "you are root user"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org -y  &>> $LOGFILE
+
+validate $? " install mongodb"
+
+systemctl enable mongod &>> $LOGFILE
+
+validate $? "enable mongodb"
+
+systemctl start mongod &>> $LOGFILE
+
+validate $? "start mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+validate $? "port updated"
+
+systemctl restart mongod &>> $LOGFILE
+
+validate $? "restart mongodb"
