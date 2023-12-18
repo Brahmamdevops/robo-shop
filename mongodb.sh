f087ae017b73a8c3e5f52b170063af448a70173c
Brahmamdevops <brahmamm32@gmail.com>
2023-12-18 14:59:42 +0530

    mongo db changes

--- mongodb.sh
+++ mongodb.sh
@@ -14,19 +14,19 @@ N="\e[0m"
 validate(){
     if [ $1 -ne 0 ]
     then
-        echo : -e " $R ... $2 error $N"
+        echo  -e " $R ... $2 error $N"
     else
-        echo : -e " $G ... $2 success $N"
+        echo  -e " $G ... $2 success $N"
     fi
 }
 
 
 if [ $ID -ne 0 ]
 then
-    echo : "please install with root user"
+    echo  "please install with root user"
     exit 1
 else
-    echo : "you are root user"
+    echo  "you are root user"
 fi
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
@@ -35,20 +35,20 @@ validate $? " coping mongo repo"
 
 dnf install mongodb-org -y  &>> $LOGFILE
 
-validate $? " install mongodb"
+validate $? " installing mongodb"
 
 systemctl enable mongod &>> $LOGFILE
 
-validate $? "enable mongodb"
+validate $? "enabling mongodb"
 
 systemctl start mongod &>> $LOGFILE
 
-validate $? "start mongodb"
+validate $? "starting mongodb"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
 
-validate $? "port updated"
+validate $? "remote access"
 
 systemctl restart mongod &>> $LOGFILE
 
-validate $? "restart mongodb"
+validate $? "restarting mongodb"
